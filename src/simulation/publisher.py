a8af7b0bb1608673cf52acba9fc9e4bca0f87bec
elisaflemer <elisaflemer@gmail.com>
2023-05-03 18:39:49 -0300

    basic turtlesim simulation

--- src/simulation/publisher.py
+++ src/simulation/publisher.py
@@ -1,9 +1,33 @@
 import rclpy
-from rclpy.node import node
+from rclpy.node import Node
 
-from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 
-class MinimalPublisher(Node):
+class TurtleController(Node):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self): 
+        super().__init__("turtle_controller")
+        self.publisher = self.create_publisher(
+            msg_type=Twist, 
+            topic="turtle1/cmd_vel", 
+            qos_profile=10
+        )
+        timer_period = 0.5
+        timer = self.create_timer(timer_period, self.publish_message)
+
+    def publish_message(self):
+        msg = Twist()
+        msg.linear.x = 1.0
+        msg.angular.z = 1.0
+        self.publisher.publish(msg)
+        self.get_logger().info("Publishing velocidades")
+
+def main(args=None):
+    rclpy.init(args=args)
+    tc = TurtleController()
+    rclpy.spin(tc)
+    tc.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()

d70c867fc3801824c551f866f14557387cf4e08b
elisaflemer <elisaflemer@gmail.com>
2023-05-09 20:37:41 -0300

    malha de controle simples

--- src/simulation/publisher.py
+++ src/simulation/publisher.py
@@ -2,25 +2,94 @@ import rclpy
 from rclpy.node import Node
 
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from tf_transformations import euler_from_quaternion
+
+MAX_DIFF = 0.1
+
+class Pose(Odometry):
+    def __init__(self, x = 0.0, y = 0.0, theta=0.0):
+        self.x = x
+        self.y = y
+        self.theta = theta
+    
+    def __repr__(self):
+        return f"x={self.x}, y={self.y}"
+    
+    def __add__(self, other):
+        return Pose(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Pose(self.x - other.x, self.y - other.y)
+
+    def __eq__(self, other):
+        return abs(self.x - other.x) < MAX_DIFF \
+            and abs(self.y - other.y) < MAX_DIFF
 
 class TurtleController(Node):
 
-    def __init__(self): 
+    def __init__(self, control_period=0.02): 
         super().__init__("turtle_controller")
-        self.publisher = self.create_publisher(
-            msg_type=Twist, 
-            topic="turtle1/cmd_vel", 
+
+        self.initiated = False
+
+        self.pose = Pose(x=0, y=0, theta=0)
+
+        self.setpoint = Pose(x=0, y=0, theta=0)
+        
+        self.control_timer = self.create_timer(
+            timer_period_sec=control_period, 
+            callback=self.control_callback
+        )
+        self.subscription = self.create_subscription(
+            msg_type=Odometry,
+            topic="odom",
+            callback=self.pose_callback,
             qos_profile=10
         )
-        timer_period = 0.5
-        timer = self.create_timer(timer_period, self.publish_message)
+        self.publisher = self.create_publisher(
+            msg_type=Twist, 
+            topic="cmd_vel", 
+            qos_profile=10
+        )
 
-    def publish_message(self):
+    def control_callback(self): 
+        if not self.initiated:
+            self.get_logger().info("Aguardando pose...")
+            return
+        
         msg = Twist()
-        msg.linear.x = 1.0
-        msg.angular.z = 1.0
+
+        if self.pose == self.setpoint:
+            self.get_logger().info("Chegou no destino!")
+            msg.linear.x = 0.0
+            exit()
+        
+        x_diff = self.setpoint.x - self.pose.x
+        y_diff = self.setpoint.y - self.pose.y
+
+        if abs(x_diff) > MAX_DIFF:
+            msg.linear.x = 1.0 if x_diff > 0 else -1.0
+
+        if abs(y_diff) > MAX_DIFF:
+            msg.linear.y = 1.0 if y_diff > 0 else -1.0
+        
         self.publisher.publish(msg)
-        self.get_logger().info("Publishing velocidades")
+
+    def pose_callback(self, msg):
+        x = msg.pose.pose.position.x
+        y = msg.pose.pose.position.y
+        z = msg.pose.pose.position.z
+        ang = msg.pose.pose.orientation
+        _, _, theta = euler_from_quaternion([ang.x, ang.y, ang.z, ang.w])
+
+        self.pose = Pose(x=x, y=y, theta=theta)
+
+        if not self.initiated:
+            self.setpoint = self.pose + Pose(x=1, y=1)
+            self.initiated = True
+
+        self.get_logger().info(f"x={x}, y={y}, theta={theta}")
 
 def main(args=None):
     rclpy.init(args=args)

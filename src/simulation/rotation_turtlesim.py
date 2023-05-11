f79cb39637685535805f92eef43f77c04a54fb3b
elisaflemer <elisaflemer@gmail.com>
2023-05-10 23:39:28 -0300

    Fila unidimensional funcionando

--- src/simulation/rotation_turtlesim.py
+++ src/simulation/rotation_turtlesim.py
@@ -4,63 +4,62 @@ from rclpy.node import Node
 
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose as TPose
+from collections import deque
 
 MAX_DIFF = 0.1
 
-
 class Pose(TPose):
-    """
-    Essa classe serve só para que eu possa modificar alguns
-    dos comportamentos default da classe Pose do turtlesim.
-    """
 
     def __init__(self, x=0.0, y=0.0, theta=0.0):
         super().__init__(x=x, y=y, theta=theta)
         
     def __repr__(self):
-        """Modifica a forma como aparece a classe no `print`"""
+
         return f"(x={self.x:.2f}, y={self.y:.2f}, theta={self.theta:.2f})"
     
     def __add__(self, other):
-        """Overload do operador `+`"""
+
         self.x += other.x
         self.y += other.y
         return self
     
     def __sub__(self, other):
-        """Overload do operador `-`"""
         self.x -= other.x
         self.y -= other.y
         return self
     
     def __eq__(self, other):
-        """
-        Modifica a funcionalidade do operador `==`
-        Passa a considerar como iguais duas poses que 
-        estiverem dentro de um range delimitado por `MAX_DIFF`.
-        A comparação considera x, y ao mesmo tempo.
-        """
-        return abs(self.x - other.x) < MAX_DIFF \
-        and abs(self.y - other.y) < MAX_DIFF
+        print(f"Comparando {self} com {other}")
+        return abs(self.x - other.x) <= MAX_DIFF
+
+class MissionControl(deque):
+    
+    def __init__(self):
+        super().__init__()
+        self.enqueue(Pose(x=4.54))
+        self.enqueue(Pose(x=7.54))
+        self.enqueue(Pose(x=4.54))
+        
+    def enqueue(self, x):
+        """Método para adicionar novos pontos ao fim da fila."""
+        super().append(x)
+    
+    def dequeue(self):
+        """Método para retirar pontos do começo da fila."""
+        return super().popleft()
 
 
 class TurtleController(Node):
-    """
-    Classe do controlador. É um nó de ROS
-    """
     
-    def __init__(self, control_period=0.02):
-        """
-        Construtor da classe controladora.
-        Aqui cria-se o publisher, a subscription e o timer.
-        Como parâmetro é possível passar o período do controlador.
-        """
+    def __init__(self, control_period=0.02, queue=MissionControl()):
         super().__init__('turtle_controller')
-        # Aproveitei a classe Pose para meus parâmetros pose e setpoint
-        # Iniciei ambos em x=-40.0 pois é um valor impossível no turtlesim
-        self.pose = Pose(x=-40.0)
-        self.setpoint = Pose(x=-40.0)
-        # O setpoint relativo é quanto a tartaruga vai andar a partir da sua origem
+
+        self.initiated = False
+
+        self.pose = Pose()
+        self.setpoint = Pose()
+        self.queue = queue
+
         self.publisher = self.create_publisher(
             msg_type=Twist,
             topic="/turtle1/cmd_vel",
@@ -72,49 +71,53 @@ class TurtleController(Node):
             callback=self.pose_callback,
             qos_profile=10
         )
-        # O timer passa a ser um timer do loop de controle.
-        # Não se deve iniciar loop de controle sem definir um período.
+
         self.control_timer = self.create_timer(
                 timer_period_sec=control_period,
                 callback=self.control_callback
         )
 
+    def update_setpoint(self):
+        """ Método responsável por buscar um novo setpoint na fila"""
+        try:
+            self.setpoint = self.queue.dequeue()
+            self.get_logger().info(f"Mbpappé chegou em {self.pose}, \
+                                   andando para {self.setpoint}")
+        except IndexError:
+            self.get_logger().info(f"Fim da jornada!")
+            exit()
+
     def control_callback(self):
-        """
-        Loop de controle. 
-        Aqui é onde define-se o acionamento dos motores do robô.
-        Roda a cada `control_period` segundos.
-        """
-        # Se a pose estiver em -40.0, significa que não chegou nenhuma info ainda
-        if self.pose.x == -40.0:
+
+        if not self.initiated:
             self.get_logger().info("Aguardando primeira pose...")
             return
         msg = Twist()
         x_diff = self.setpoint.x - self.pose.x
-        # y_diff = self.setpoint.y - self.pose.y
+
+        #self.get_logger().info(f"x_diff: {x_diff}")
+        #self.get_logger().info(f"Pose: {self.pose}")
+
         if self.pose == self.setpoint:
-            msg.linear.x, msg.linear.y = 0.0, 0.0
+            self.get_logger().info("To aqui")
+            msg.linear.x = 0.0
             self.get_logger().info(f"Mbappé chegou em {self.setpoint}")
-            exit()
-        # if abs(y_diff) > MAX_DIFF:
-        #     msg.linear.y = 0.5 if y_diff > 0 else -0.5
-        # else:
-        #     msg.linear.y = 0.0
+            self.update_setpoint()
+     
         if abs(x_diff) > MAX_DIFF:
+            self.get_logger().info(f"Distância para o setpoint: {x_diff}")
             msg.linear.x = 0.5 if x_diff > 0 else -0.5
+        
         self.publisher.publish(msg)
         
     def pose_callback(self, msg):
-        """
-        Método de callback da subscrição à pose da tartaruga.
-        Toda vez que chega uma mensagem no tópico de pose,
-        esse método é executado.
-        """
+
         self.pose = Pose(x=msg.x, y=msg.y, theta=msg.theta)
-        # Se for a primeira vez passando por aqui, cria o setpoint.
-        if self.setpoint.x == -40.0:
+
+        if not self.initiated:
+            self.initiated = True
             self.get_logger().info(f"Primeira pose recebida: {self.pose}")
-            self.setpoint = self.pose + Pose(x=1.0)
+            self.update_setpoint()
             self.get_logger().info(f"Setpoint: {self.setpoint}")
 
 
@@ -124,7 +127,6 @@ def main(args=None):
     rclpy.spin(tc)
     tc.destroy_node()
     rclpy.shutdown()
-    
 
 if __name__ == "__main__":
     main()

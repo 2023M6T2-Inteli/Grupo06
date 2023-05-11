88582de659d90e8937321161a1d8f810a9207805
jeanroths <jean.machado@sou.inteli.edu.br>
2023-05-11 20:36:24 -0300

    começando a implementar fila

--- src/simulation/turtlebot_rotation.py
+++ src/simulation/turtlebot_rotation.py
@@ -7,6 +7,7 @@ from nav_msgs.msg import Odometry
 from tf_transformations import euler_from_quaternion
 from turtlesim.msg import Pose as TPose
 import math
+from collections import deque
 
 MAX_DIFF = 0.1
 
@@ -44,9 +45,36 @@ class Rotation(TPose):
     
     def __eq__(self, other):
         return abs(self.theta - other.theta) <= MAX_DIFF
+
+
+
+class MissionControl(deque):
+    
+    def __init__(self):
+        """
+        No construtor do controle de missão é preciso definir o arquivo
+        csv de pontos a serem lidos. A partir daí, o construtor inicia o deque
+        e faz a leitura do arquivo csv, adicionando cada ponto na fila.
+        """
+        super().__init__()
+        #self.enqueue(Pose(1.0, 1.0))
+        self.enqueue(Pose(1.0, -1.0))
+        self.enqueue(Pose(-1.0, -1.0))
+        self.enqueue(Pose(-1.0, 1.0))
+        self.enqueue(Pose(0.0, 0.0))
+        
+    def enqueue(self, x):
+        """Método para adicionar novos pontos ao fim da fila."""
+        super().append(x)
+    
+    def dequeue(self):
+        """Método para retirar pontos do começo da fila."""
+        return super().popleft()
+
+
     
 class BotController(Node):
-    def __init__(self, control_period=0.02):
+    def __init__(self, control_period=0.02, mission_control=MissionControl()):
         super().__init__("bot_controller")
 
         self.initiated = False
@@ -56,6 +84,7 @@ class BotController(Node):
         self.setpoint_rotation = Rotation()
         self.setpoint_translation = 0.0
         self.current_rotation = Rotation()
+        self.queue = mission_control
         
         self.control_timer = self.create_timer(
             timer_period_sec=control_period, 
@@ -96,7 +125,7 @@ class BotController(Node):
                 msg.linear.x = 0.0
                 self.get_logger().info(f"Donatello chegou ao destino")
                 self.publisher.publish(msg)
-                exit()
+                self.update_setpoint()
             else:
                 offset = self.setpoint_rotation.theta - self.current_rotation.theta
                 print(f"offset: {offset}")
@@ -114,28 +143,33 @@ class BotController(Node):
         self.publisher.publish(msg)
 
     def update_setpoint(self):
-        self.setpoint = Pose(-1.0, 1.0)
+        try:
+            self.setpoint = self.queue.dequeue()
+            self.get_logger().info(f"Mbpappé chegou em {self.pose}, \
+                                   andando para {self.setpoint}")
+            if self.setpoint == Pose(0.0,0.0):
+                self.theta = Rotation(theta=0.0) 
+            else:
+                self.theta= Rotation(theta=math.atan2(self.setpoint.y - self.pose.y, self.setpoint.x - self.pose.x))
+                print(f"theta: {self.theta}")
 
-        if self.setpoint == Pose(0.0,0.0):
-            self.theta = Rotation(theta=0.0) 
-        else:
-            self.theta= Rotation(theta=math.atan2(self.setpoint.y - self.pose.y, self.setpoint.x - self.pose.x))
-            print(f"theta: {self.theta}")
+            self.relative_vector = Pose(x=self.setpoint.x - self.pose.x, y=self.setpoint.y - self.pose.y)
+            self.relative_translation = math.sqrt(self.relative_vector.x**2 + self.relative_vector.y**2)
 
-        self.relative_vector = Pose(x=self.setpoint.x - self.pose.x, y=self.setpoint.y - self.pose.y)
-        self.relative_translation = math.sqrt(self.relative_vector.x**2 + self.relative_vector.y**2)
+            if self.relative_vector.x >= 0 and self.relative_vector.y >=0:
+                self.setpoint_rotation = Rotation(theta=abs(self.theta.theta))
 
-        if self.relative_vector.x >= 0 and self.relative_vector.y >=0:
-            self.setpoint_rotation = Rotation(theta=abs(self.theta.theta))
+            elif self.relative_vector.x >=0 and self.relative_vector.y <= 0:
+                self.setpoint_rotation = Rotation(theta=-abs(self.theta.theta))
 
-        elif self.relative_vector.x >=0 and self.relative_vector.y <= 0:
-            self.setpoint_rotation = Rotation(theta=-abs(self.theta.theta))
-
-        elif self.relative_vector.x <=0 and self.relative_vector.y <= 0:
-            self.setpoint_rotation = Rotation(theta=-abs(self.theta.theta))
-            print(f"setpoint_rotation: {self.setpoint_rotation}")
-        else:
-            self.setpoint_rotation = Rotation(theta=abs(self.theta.theta))
+            elif self.relative_vector.x <=0 and self.relative_vector.y <= 0:
+                self.setpoint_rotation = Rotation(theta=-abs(self.theta.theta))
+                print(f"setpoint_rotation: {self.setpoint_rotation}")
+            else:
+                self.setpoint_rotation = Rotation(theta=abs(self.theta.theta))
+        except IndexError:
+            self.get_logger().info(f"Fim da jornada!")
+            exit()
 
     def pose_callback(self, msg):
         x = msg.pose.pose.position.x

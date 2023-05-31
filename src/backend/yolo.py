9b1c9a8653b6334bcadc3da85f2b305cd4d258d4
jeanroths <jean.machado@sou.inteli.edu.br>
2023-05-31 17:15:27 -0300

    "comentarios"

--- src/backend/yolo.py
+++ src/backend/yolo.py
@@ -4,12 +4,15 @@ import cv2
 model = YOLO("best.pt")
 
 def get_yolo_results():
-    cap = cv2.VideoCapture(0)
+    # Open the video file - webcam
+    cap = cv2.VideoCapture(1)
 
     while True:
+        # Read a frame from the video
         ret, frame = cap.read()
         results = model(frame)
 
+        # Visualize the results on the frame
         annotated_frame = results[0].plot()
 
         # cv2.imshow("YOLOv8 Inference", annotated_frame)
@@ -28,36 +31,3 @@ def get_yolo_results():
             b'--frame\r\n'
             b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n\r\n'
         )
-    # imagem = cv2.imread(img)
-    # results = model(imagem)
-    # annotated_frame = results[0].plot()
-    # cv2.imshow("YOLOv8 Inference", annotated_frame)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-
-
-
-# Open the video file
-
-
-# Loop through the video frames
-# while cap.isOpened():
-    # Read a frame from the video
-# frame = cv2.imread("download (1).jpeg")
-
-#         # Run YOLOv8 inference on the frame
-# results = model(frame)
-
-# # Visualize the results on the frame
-# annotated_frame = results[0].plot()
-
-# # Display the annotated frame
-# cv2.imshow("YOLOv8 Inference", annotated_frame)
-
-# # Break the loop if 'q' is pressed
-# cv2.waitKey(0)
-
-
-# Release the video capture object and close the display window
-
-# cv2.destroyAllWindows()

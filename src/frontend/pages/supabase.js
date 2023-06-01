b8fb3800767e0a5c320a8060f15b47b5a2ff3f70
Gabriela Rodrigues <99189378+gabInteli@users.noreply.github.com>
2023-06-01 17:10:44 -0300

    create supabase

---
+++ src/frontend/pages/supabase.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import * as React from 'react';
+import { SupaBase } from '../src/components/SupaBase';
+
+export default function Home() {
+  return (
+     <div>
+      <SupaBase></SupaBase>
+    </div>
+  )
+}
---
+++ src/frontend/src/components/SupaBase/SupaBase.jsx
@@ -0,0 +1,37 @@
+import React, {useState} from 'react'
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+    process.env.SUPABASE_PROJECT_URL,
+    process.env.SUPABASE_ANON_KEY
+  );
+
+export default function SupaBase() {
+    const [file, setfile] = useState([]);
+  
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const filename = `${uuidv4()}-${file.name}`;
+    
+        const { data, error } = await supabase.storage
+          .from("images")
+          .upload(filename, file, {
+            cacheControl: "3600",
+            upsert: false,
+          });
+    
+        const filepath = data.path;
+        // save filepath in database
+      };
+  
+    const handleFileSelected = (e) => {
+      setfile(e.target.files[0]);
+    };
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <input type="file" name="image" onChange={handleFileSelected} />
+        <button type="submit">Upload image</button>
+      </form>
+    );
+  }

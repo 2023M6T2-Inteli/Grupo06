e173c67b6d4a84a2473bd04c61460429606e0ea5
Leao09 <99265654+Leao09@users.noreply.github.com>
2023-06-06 18:02:27 -0300

    backend + imgs

---
+++ src/backend/schemas.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from typing import List
+from pydantic import BaseModel
+class ReportBaseSchema(BaseModel):
+    id: str | None = None
+    projectName: str
+    typePlace: int
+    operator: str | None = None
+    date: datetime
+    gasLevel1: float
+    gasLevel2: float
+    createdAt: datetime | None = None
+    updatedAt: datetime | None = None
+    class Config:
+        orm_mode = True
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+class ListReportResponse(BaseModel):
+    status: str
+    results: int
+    notes: List[ReportBaseSchema]
--- src/backend/yolo.py
+++ src/backend/yolo.py
@@ -1,6 +1,5 @@
 from ultralytics import YOLO 
 import cv2
-
 model = YOLO("best.pt")
 
 def get_yolo_results():

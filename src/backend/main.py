1d45d1a7eb33014599a88f979573dbb7c2c73622
Gabriela Rodrigues <99189378+gabInteli@users.noreply.github.com>
2023-06-12 10:24:45 -0300

    update opencv

--- src/backend/main.py
+++ src/backend/main.py
@@ -20,6 +20,12 @@ from typing import List, Tuple
 import uvicorn 
 from yolo import get_yolo_results
 
+# from database import Base
+# from sqlalchemy import TIMESTAMP, Column, String, Boolean, create_engine, Integer, Date, Float
+# from sqlalchemy.sql import func
+# from fastapi_utils.guid_type import GUID, GUID_DEFAULT_SQLITE
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
 
 # Cria o servidor
 app = fastapi.FastAPI()

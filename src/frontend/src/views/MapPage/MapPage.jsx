639d3bc1faf5771f897001f98a89d2a02abe3997
IgorSFG <igor.garcia@sou.inteli.edu.br>
2023-06-15 17:16:54 -0300

    get supabase

--- src/frontend/src/views/MapPage/MapPage.jsx
+++ src/frontend/src/views/MapPage/MapPage.jsx
@@ -3,31 +3,44 @@ import { Row, Col, Table, Card, Button} from "antd";
 import { NavBar } from "../../components/NavBar";
 import DownloadIcon from "@mui/icons-material/Download";
 import BotControllerNav from "../../../../simulation/.vscode";
+import {createClient} from "@supabase/supabase-js";
+
+const url = "https://etxrmfvkgcrpyzdpvvrn.supabase.co"
+const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV0eHJtZnZrZ2NycHl6ZHB2dnJuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4NjY4MDk4MCwiZXhwIjoyMDAyMjU2OTgwfQ.BosLYjeNsyNMhs88FlV_PNzH9QRBqeBgAUJJU4SzlQw"
+const supabase = createClient(url, key)
 
 export default function MapPage() {
   const Canvas = () => {
-    
+
     const canvasRef = useRef(null);
     const canvas_width = 800;
     const canvas_height = 600;
 
-    let position = 0
+    let position_X = 0
+    let position_Y = 0
     
     useEffect(() => {
       const canvas = canvasRef.current
-      const ctx = canvas.getContext('2d')
+      const ctx = canvas.getContext('2d');
 
-      var img = new Image();
-      img.src = "down-arrow.png";
-
-      const draw_player = (pos_x, pos_y) => {
+      const draw_player = (px, py) => {
         ctx.fillStyle = 'green';
-        ctx.fillRect(pos_x, pos_y, 20, 20);
+        ctx.fillRect(px, py, 20, 20);
+      }
+
+      const get_position = async () => {
+        
+        const { data, error } = await supabase
+        .from('Coordinates')
+        .select()
+
+        console.log(data)
+
       }
       
       const animation = () => {
         ctx.clearRect(0, 0, canvas_width, canvas_height);
-        draw_player(pos_x, pos_y);
+        draw_player(position_X, position_Y);
         requestAnimationFrame(animation);
       }
 
--- src/simulation/nav2/BotController.py
+++ src/simulation/nav2/BotController.py
@@ -9,48 +9,66 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from tf_transformations import euler_from_quaternion
+from supabase import Client, create_client
 import rclpy
 
 from Pose import Pose
 
 
+# Credenciais do Supabase
+url: str = "https://etxrmfvkgcrpyzdpvvrn.supabase.co"
+key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV0eHJtZnZrZ2NycHl6ZHB2dnJuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4NjY4MDk4MCwiZXhwIjoyMDAyMjU2OTgwfQ.BosLYjeNsyNMhs88FlV_PNzH9QRBqeBgAUJJU4SzlQw"
+supabase: Client = create_client(url, key)
+
+
 # Define a classe BotController, que representa o nó de controle do robô
 class BotController(Node):
     # Inicializa o nó com período de controle de 0.05s e uma fila vazia
-    def __init__(self, control_period=0.05):
+    def __init__(self, control_period=0.2):
         super().__init__("bot_controller")
-
-        self.pose = Pose()
 
         # Cria um assinante para receber a pose do robô
         self.subscription = self.create_subscription(
             msg_type=Odometry,
             topic="odom",
-            callback=self.pose_callback,
+            callback=self.odom_callback,
             qos_profile=10
         )
 
-    # Callback para receber pose atual
-    def pose_callback(self, msg):
+        self.odom_data = None
 
-            # Decompõe a mensagem de pose em x, y e theta
-            x = msg.pose.pose.position.x
-            y = msg.pose.pose.position.y
-            z = msg.pose.pose.position.z
-            ang = msg.pose.pose.orientation
+        self.timer = self.create_timer(control_period, self.timer_callback)
+
+        # Função que recebe os dados de odometria do robô burguer
+    def odom_callback(self, msg):
+        self.odom_data = msg
+
+    # Callback para receber pose atual
+    def timer_callback(self):
+        if self.odom_data is not None:
+
+            # Decompõe a mensagem de pose em x, y, z e theta
+            x = self.odom_data.pose.pose.position.x
+            y = self.odom_data.pose.pose.position.y
+            z = self.odom_data.pose.pose.position.z
+            ang = self.odom_data.pose.pose.orientation
             _, _, theta = euler_from_quaternion([ang.x, ang.y, ang.z, ang.w])
 
             # Atualiza a pose e rotação atuais
             self.current_pose = Pose(x=x, y=y, theta=theta)
 
+            try:
+                data, count = supabase.table('Coordinates').insert({"x": x, "y": y, "theta": theta}).execute()
+            except Exception:
+                print("Erro ao inserir dados no banco de dados.")
+                data, count = supabase.table('Coordinates').delete().eq("x", x).execute()
+            
+
 
     
-def main():
-    rclpy.init()
-    tc = BotController()
-    rclpy.spin(tc)
 
-if __name__ == "__main__":
-    main()
+rclpy.init()
+tc = BotController()
+rclpy.spin(tc)
 
  

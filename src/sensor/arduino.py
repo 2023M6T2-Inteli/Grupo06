bb09c071b62e57c14da9e0b5eb66af8a691cf040
jeanroths <jean.machado@sou.inteli.edu.br>
2023-06-19 22:53:02 -0300

    códigos sensor pegar valores

---
+++ src/sensor/arduino.py
@@ -0,0 +1,84 @@
+import serial
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Configurações da porta serial
+porta = 'COM4'  # Substitua pela porta COM correta
+baud_rate = 9600
+
+# Inicializa a comunicação serial
+conexao = serial.Serial(porta, baud_rate)
+
+# Lista para armazenar os valores analógicos
+valores_analogicos = []
+
+# Lista para armazenar as médias móveis
+medias_moveis = []
+
+# Número de amostras para calcular a média móvel
+janela = 5
+
+# Lista para armazenar as médias móveis por porcentagem
+media_mov_porc = []
+
+# Configura o gráfico
+fig, ax = plt.subplots()
+
+# variável para controlar o loop
+continuar = True
+
+try:
+    # Lê os valores analógicos continuamente
+    while continuar:
+        # Lê uma linha da porta serial
+        linha = conexao.readline().decode().strip()
+
+        # Verifica se a linha contém um valor analógico válido
+        if linha.startswith('Leitura'):
+            valor_analogico = int(linha.split(':')[1])
+            print('Valor analógico:', valor_analogico)
+
+            # Calcula a média móvel usando a função np.mean()
+            media_movel = np.mean(valores_analogicos[-janela:])
+
+            # faz a média móvel em porcentagem
+            porcentagem = (media_movel * 100) / 1023
+
+            print('Média móvel:', media_movel)
+            print(f'(Média móvel em porcentagem: {porcentagem:.2f}%)')
+            # Armazena o valor analógico na lista
+            valores_analogicos.append(valor_analogico)
+
+            # Verifica se há valores suficientes para calcular a média móvel
+            if len(valores_analogicos) >= janela:
+
+                # Armazena a média móvel na lista
+                medias_moveis.append(media_movel)
+                media_mov_porc.append(porcentagem)
+
+            if len(valores_analogicos) >= 30:
+                continuar = False
+
+            # Atualiza o gráfico com os valores atuais e as médias móveis
+            ax.clear()
+            #ax.plot(valores_analogicos, label='Valores Analógicos')
+            #ax.plot(medias_moveis, label='Médias Móveis')
+            ax.plot(media_mov_porc, label='Médias Móveis em Porcentagem')
+            ax.legend()
+
+            # Define os rótulos e título do gráfico
+            ax.set_xlabel('Amostras')
+            ax.set_ylabel('Valor')
+            ax.set_title('Valores Analógicos e Médias Móveis')
+
+            # Exibe o gráfico
+            plt.pause(0.01)
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    # Fecha a conexão serial
+    conexao.close()
+
+plt.show()
